904274a5e1c2023fea98de6d7ee44c924c0a4149
JKH <jyyblue1987@outlook.com>
2016-08-24 12:40:16 +0800

    [add] show country list.

--- Source/SimpleClock/app/src/main/java/com/timezone/simpleclock/MainActivity.java
+++ Source/SimpleClock/app/src/main/java/com/timezone/simpleclock/MainActivity.java
@@ -4,16 +4,22 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 import java.util.TimeZone;
 
 import common.list.adapter.ItemCallBack;
@@ -23,6 +29,7 @@ import common.list.adapter.ViewHolder;
 public class MainActivity extends AppCompatActivity {
     ListView m_listItems	= null;
     MyListAdapter m_adapterList = null;
+    ArrayList<String> m_countryList = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,10 +48,48 @@ public class MainActivity extends AppCompatActivity {
 
     private void initData()
     {
+        getCountryList();
         getLocalTime();
         getTimeZone();
     }
 
+    private void getCountryList()
+    {
+        Locale[] locales = Locale.getAvailableLocales();
+        ArrayList<String> countries = new ArrayList<String>();
+        for (Locale locale : locales) {
+            String country = locale.getDisplayCountry();
+            if (country.trim().length()>0 && !countries.contains(country)) {
+                countries.add(country);
+            }
+        }
+        Collections.sort(countries);
+
+        m_countryList = new ArrayList<String>();
+        for (String country : countries) {
+            System.out.println(country);
+            m_countryList.add(country);
+        }
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_dropdown_item, m_countryList);
+        //스피너 속성
+        Spinner sp = (Spinner) this.findViewById(R.id.spinner_country);
+        sp.setPrompt("Select Country"); //
+        sp.setAdapter(adapter);
+        sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, m_countryList.get(i), Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
     private void getLocalTime()
     {
         Date date = new Date();
